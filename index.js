c039156bc21f6b5b4907044218a608f4413afdb0
Federico Aloi <federico.aloi@gmail.com>
2020-03-10 18:31:34 -0300

    Consignas y ejemplo

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const superagent = require('superagent');
 
+function imprimirMuseos(error, respuesta) {
+  if (error) {
+    throw new Error('algo se rompió', error);
+  }
+
+  const cantidad = respuesta.body.count;
+  const museos = respuesta.body.results;
+
+  console.log(`Se encontraron ${cantidad} museos.`);
+  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+}
+
+console.log('Antes de llamar a superagent')
+
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end((err, res) => {
-    const cantidad = res.body.count
-    const museos = res.body.results
+  .end(imprimirMuseos)
 
-    console.log(`¡Se encontraron ${cantidad} museos!`)
-    console.log('El primero es', museos[0])
-  })
-
-
-// Consignas
-// 1. Escribir a un archivo.
-// 2. Buscar latitud y longitud y luego escribir a un archivo.
-// 3. Idem, con promises.
+console.log('Después de llamar a superagent')
